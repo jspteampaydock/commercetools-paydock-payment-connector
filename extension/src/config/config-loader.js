0493b95fea7d5de710b25a0c34c9bb9fb507c194
jspteampaydock <157017480+jspteampaydock@users.noreply.github.com>
2024-06-06 07:07:16 +0300

    Change readme files

--- extension/src/config/config-loader.js
+++ extension/src/config/config-loader.js
@@ -13,11 +13,9 @@ function loadFromPaydockIntegrationEnvVar() {
   try {
     return JSON.parse(process.env.PAYDOCK_INTEGRATION_CONFIG)
   } catch (e) {
-    // eslint-disable-next-line max-len
-    return  {clientId: 'nb1cYFUNQWcRcTPBfCvxIGnJ', clientSecret: '4X3F6IpnQtRwrfq2DOMt_ozkLRE3mfJB', projectKey: 'zh-test-powerboard', apiUrl: 'https://api.europe-west1.gcp.commercetools.com', authUrl: 'https://auth.europe-west1.gcp.commercetools.com'}
-    /*throw new Error(
-      `Paydock integration${e} configuration is not provided in the JSON format${process.env.PAYDOCK_INTEGRATION_CONFIG}`,
-    )*/
+    throw new Error(
+        `Paydock integration configuration is not provided in the JSON format`,
+    )
   }
 }
 
--- notification/src/config/config-loader.js
+++ notification/src/config/config-loader.js
@@ -13,8 +13,7 @@ function loadFromPaydockIntegrationEnvVar() {
     return JSON.parse(process.env.PAYDOCK_INTEGRATION_CONFIG)
   } catch (e) {
     throw new Error(
-        // eslint-disable-next-line max-len
-        `Paydock integration ${e} configuration is not provided in the JSON format${ process.env.PAYDOCK_INTEGRATION_CONFIG}`,
+        `Paydock integration configuration is not provided in the JSON format`,
     )
   }
 }
